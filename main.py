80ca87382056137dbd382b3a01958a1b189d672d
Moavia Mahmood <moaviamahmood@gmail.com>
2026-02-15 18:28:57 +0300

    .env added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from etl.extract import extract_jobs
 from etl.transform import transform_bronze_to_silver
 from etl.load import load_csv_to_postgres
 import json
+from dotenv import load_dotenv
+import os
 
 BASE_URL = "https://www.arbeitnow.com/api/job-board-api"
 
@@ -28,14 +30,15 @@ print("Silver layer created.")
 # =============================
 #  Load to PostgreSQL Locally
 # =============================
+load_dotenv()
 load_csv_to_postgres(
     csv_path="data/silver_jobs.csv",
     table_name="jobs_silver",
-    pg_username="postgres",
-    pg_password="mavi123",   
-    pg_host="localhost",
-    pg_port="5432",
-    pg_db="jobs_db"
+    pg_username=os.getenv("DB_USERNAME"),
+    pg_password=os.getenv("DB_PASSWORD"),   
+    pg_host=os.getenv("DB_HOST"),
+    pg_port=os.getenv("DB_PORT"),
+    pg_db=os.getenv("DB_NAME")
 )
 
 print("Data loaded to Localhost PostgreSQL.")
@@ -47,11 +50,11 @@ print("Data loaded to Localhost PostgreSQL.")
 load_csv_to_postgres(
     csv_path="data/silver_jobs.csv",
     table_name="jobs_silver",
-    pg_username="postgres",
-    pg_password="mavi123",  
-    pg_host="localhost",
-    pg_port="5432",
-    pg_db="jobs_db"
+    pg_username=os.getenv("DB_USERNAME"),
+    pg_password=os.getenv("DB_PASSWORD"),  
+    pg_host=os.getenv("DB_HOST"),
+    pg_port=os.getenv("DB_PORT"),
+    pg_db=os.getenv("DB_NAME")
 )
 
 print("Data loaded to Docker PostgreSQL.")
